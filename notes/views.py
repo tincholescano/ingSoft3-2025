8fbc9980e9c3b15a901c8b2888aa401b88e6132b
tincholescano <lescanomartin.ml@gmail.com>
2025-02-13 11:46:30 -0300

    Nuevo test desde Django

--- notes/views.py
+++ notes/views.py
@@ -52,4 +52,4 @@ class NoteCreateView(CreateView):
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "registration/signup.html"
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("note_list")
