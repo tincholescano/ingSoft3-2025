aeffbfb5c2c4f9c2413c3ef6661caa08590ea3d0
tincholescano <lescanomartin.ml@gmail.com>
2025-02-14 09:25:30 -0300

    Deployment intent

--- notepad/settings.py
+++ notepad/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-)l0$%hi$=k)zxop)*jnpg28zkg891rtm!u9)#pfpbx5@e+!tti
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "gc-ingsoft3-2025-921103043800.southamerica-east1.run.app",
-    "localhost"
+    ".southamerica-east1.run.app",
+    "*"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
